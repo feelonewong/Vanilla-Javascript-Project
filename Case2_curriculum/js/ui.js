a7209aa1e986325d809517949a486cdd8232e3ff
feelonewong <feelonewong@gmail.com>
2020-06-04 17:39:44 +0800

    ecapsulate the message component

--- Case2_curriculum/js/ui.js
+++ Case2_curriculum/js/ui.js
@@ -10,9 +10,40 @@
             })
             messageDOM.innerHTML = params.messageText;
             body.appendChild(messageDOM);
+            show(messageDOM);
         }
         window.UI.$message = $message;
 
+        function show(node){
+            let timer = null;
+            timer = setTimeout(function(){
+                node.classList.add('show')
+                clearTimeout(timer);
+            },500)
+            hide(node);
+        }
+        window.UI.show = show;
+
+        function hide(node){
+            let timer = null;
+            timer = setTimeout(function(){
+                node.classList.remove('show')
+                clearTimeout(timer);
+                remove(node);
+            },1000)
+        }
+        window.UI.hide = hide;
+        
+        function remove(node){
+            let timer = null;
+            let body = document.getElementsByTagName('body')[0]
+            timer = setTimeout(function(){
+                body.removeChild(node);
+                clearTimeout(timer);
+            },1000)
+        }
+        window.UI.hide = hide;
+        
     } else {
         throw new Error("Error");
     }
